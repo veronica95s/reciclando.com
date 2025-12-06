ff64ecf0318b941e65dfa5d01de3b856a00919bc
Vitória Ferreira <vitoriasouzaf@hotmail.com>
2025-12-06 13:50:19 -0300

    feat: add adcreation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HowToRecycle from "./pages/HowToRecycle";
-import Login from "./pages/Login";
-// import Ads from "./pages/Ads";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HowToRecycle from './pages/HowToRecycle';
+import Login from './pages/Login';
+import Ads from './pages/Ads';
+import AdCreation from './pages/AdCreation';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/how-to-recycle" element={<HowToRecycle />} />
-        <Route path="/anuncios" element={<Ads />}> </Route>
+        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/como-reciclar' element={<HowToRecycle />} />
+        <Route path='/anuncios' element={<Ads />} />
+        <Route path='/anuncios/novo' element={<AdCreation />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/AdForm/AdForm.jsx
@@ -0,0 +1,198 @@
+import Categories from '../Categories/Categories';
+import styles from './AdForm.module.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import STATES_LIST from '../../utils/statesList';
+
+export default function Form() {
+  const [categories, setCategories] = useState([]);
+  const [image, setImage] = useState('');
+  const [formData, setFormData] = useState({
+    image: '',
+    title: '',
+    description: '',
+    category: [],
+    donorId: 1,
+    city: '',
+    state: '',
+    postalCode: '',
+    phone: '',
+    email: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    formData.category = categories;
+
+    try {
+      const response = await axios.post(
+        'http://localhost:8081/api/v1/ads/new',
+        formData
+      );
+      console.log('Dados do formulário enviados com sucesso:', response.data);
+    } catch (error) {
+      console.error('Erro ao enviar dados do formulário:', error);
+    }
+  };
+
+  return (
+    <div className={styles['form-container']} style={{ maxWidth: '800px' }}>
+      <h2>Informações do Anúncio</h2>
+      <p>
+        Preencha os dados abaixo para criar seu anúncio de materiais recicláveis
+      </p>
+      <form className='row g-3' onSubmit={handleSubmit}>
+        <div className='col-md-12'>
+          <label htmlFor='title' className='form-label'>
+            Título do Anúncio
+          </label>
+          <input
+            type='text'
+            className='form-control'
+            name='title'
+            id='title'
+            value={formData.title}
+            onChange={handleChange}
+            placeholder='Ex: Papelão e garrafas PET para doação'
+          />
+        </div>
+        <div className='col-md-12'>
+          <label htmlFor='description' className='form-label'>
+            Descrição
+          </label>
+          <textarea
+            className='form-control'
+            placeholder='Descreva os materiais que você tem disponível, quantidade aproximada e qualquer informação adicional relevante...'
+            id='description'
+            name='description'
+            value={formData.description}
+            onChange={handleChange}
+            style={{ height: '120px' }}
+          ></textarea>
+        </div>
+        <div className='col-md-12'>
+          <label htmlFor='adImage' className='form-label'>
+            Foto do Material
+          </label>
+          <input
+            type='file'
+            name='adImage'
+            className='form-control'
+            id='adImage'
+            onChange={(e) => setImage(e.target.files[0])}
+          />
+        </div>
+        <div className='col-md-12 mb-0'>
+          <label htmlFor='category' className='form-label'>
+            Categoria
+          </label>
+          <Categories
+            categories={categories}
+            onCategoriesChange={setCategories}
+            showAll={false}
+          />
+        </div>
+        <div className={styles['location-info']}>
+          <h3>Localização</h3>
+          <div className='row'>
+            <div className='col-md-5 mt-0'>
+              <label htmlFor='city' className='form-label'>
+                Cidade
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                id='city'
+                name='city'
+                value={formData.city}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='col-md-4 mt-0'>
+              <label htmlFor='state' className='form-label'>
+                Estado
+              </label>
+              <select
+                id='state'
+                className='form-select'
+                name='state'
+                onChange={handleChange}
+                value={formData.state}
+              >
+                <option value='' disabled>
+                  Selecione uma opção...
+                </option>
+                {STATES_LIST.map((uf, idx) => (
+                  <option key={idx} value={uf}>
+                    {uf}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className='col-md-3 mt-0'>
+              <label htmlFor='postalCode' className='form-label'>
+                CEP
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                id='postalCode'
+                name='postalCode'
+                value={formData.postalCode}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div className={styles['contact-info']}>
+          <h3>Informações de Contato</h3>
+          <div className='row gap-2 mb-0'>
+            <div className='col-md-5'>
+              <label htmlFor='phone' className='form-label'>
+                Telefone
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                id='phone'
+                placeholder='(00) 00000-0000'
+                name='phone'
+                value={formData.phone}
+                onChange={handleChange}
+              />
+            </div>
+            <div className='col-md'>
+              <label htmlFor='email' className='form-label'>
+                E-mail
+              </label>
+              <input
+                type='text'
+                className='form-control'
+                id='email'
+                placeholder='seu@email.com'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+        </div>
+        <div
+          className={`col-12 d-flex align-item-center gap-4 ${styles['button-wrapper']}`}
+        >
+          <button className={`btn ${styles['btn-secondary']}`} type='button'>
+            Cancelar
+          </button>
+          <button type='submit' className='btn btn-success'>
+            Publicar Anúncio
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
