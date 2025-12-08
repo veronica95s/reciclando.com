c2b3a6bfd2be2d472620c4a7172df723078001cf
MarcelleDev <devmarcelle94@gmail.com>
2025-12-08 18:21:35 -0300

    feat(frontend): implementa página completa do perfil do reciclador

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
+import Recyclers from "./pages/Recyclers/Recyclers"; 
 import RecyclerProfile from "./pages/RecyclerProfile/RecyclerProfile";
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/reciclador" element={<RecyclerProfile />} />
+        <Route path="/recicladores" element={<Recyclers />} />  
+        <Route path="/reciclador" element={<RecyclerProfile />} /> 
       </Routes>
 
       <Footer />
